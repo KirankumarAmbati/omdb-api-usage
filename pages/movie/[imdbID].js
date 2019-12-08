5abd7971358b49fd0b484e0e820a9326aff553d6
KirankumarAmbati <kiran.chinna12520@gmail.com>
2019-12-08 12:52:04 +0530

    chore: css modifications

--- pages/movie/[imdbID].js
+++ pages/movie/[imdbID].js
@@ -13,18 +13,20 @@ const Movie = ({ movie }) => {
             <div className="bg-gray-light p-8">
                 <div className="bg-red-300 p-8 flex">
                     <img className="p-2 border-black border-2 border-solid" src={movie.Poster} alt={movie.Title} />
-                    <div className="mx-10">
-                        <h1 className="text-4xl">{`${movie.Title} (${movie.Year})`}</h1>
-                        <span className="px-3 border-solid border-black border-r-2 border-l-2">{(movie.Type).toUpperCase()}</span>
-                        <span className="px-3 border-solid border-black border-r-2">{movie.Genre}</span>
-                        <span className="px-3 border-solid border-black border-r-2">{movie.Runtime}</span>
-                        <span className="px-3 border-solid border-black border-r-2">{movie.Language}</span><br />
-                        <div className="my-2">
-                            <span><b>IMDB:</b> {movie.imdbRating} </span><br />
-                            <span><b>Rotten Tomatoes:</b> {movie.Ratings[1].Value}</span>
+                    <div className="mx-10 border-black border-2 border-solid p-4">
+                        <h1 className="text-4xl px-4 bg-black text-red-400">{`${movie.Title} (${movie.Year})`}</h1>
+                        <div className="mt-4 flex">
+                            <span className="px-3 flex-grow text-center border-solid border-black border-r-2 border-l-2">{(movie.Type).toUpperCase()}</span>
+                            <span className="px-3 flex-grow text-center border-solid border-black border-r-2">{movie.Genre}</span>
+                            <span className="px-3 flex-grow text-center border-solid border-black border-r-2">{movie.Runtime}</span>
+                            <span className="px-3 flex-grow text-center border-solid border-black border-r-2">{movie.Language}</span>
                         </div>
-                        <p className="my-2">{movie.Plot}</p>
-                        <table>
+                        <div className="my-2">
+                            <span><b>IMDB:</b> {movie.imdbRating} </span>
+                            <span className="float-right"><b>Rotten Tomatoes:</b> {movie.Ratings[1].Value}</span>
+                        </div>
+                        <p className="my-2 mt-8">{movie.Plot}</p>
+                        <table className="mt-8">
                             <tbody>
                                 <tr>
                                     <td>
